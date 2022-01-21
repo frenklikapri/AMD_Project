399b2b43d5a5c7738ad93db9b731d96c5e217800
Frenkli Kapri <kaprifrenkli@gmail.com>
2022-01-21 20:15:34 +0100

    Dummy data

---
+++ Movies/Dtos/FilmDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Dtos
+{
+    public class FilmDto
+    {
+        public FilmDto()
+        {
+
+        }
+
+        public FilmDto(Guid id, string title, DateOnly releaseDate)
+        {
+            Id = id;
+            Title = title;
+            ReleaseDate = releaseDate;
+        }
+
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
---
+++ Movies/Pages/Films.razor.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Movies.Dtos;
+
+namespace Movies.Pages
+{
+    public partial class Films
+    {
+        [Inject]
+        public IJSRuntime JS { get; set; }
+
+        private FilmDto _filmToSave = new();
+        private IJSObjectReference _module;
+        private List<FilmDto> _films = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            _films = new List<FilmDto>
+            {
+                new FilmDto(Guid.NewGuid(), "The Battle at Lake Changjin", new DateOnly(2021, 9, 21)),
+                new FilmDto(Guid.NewGuid(), "No Time to Die", new DateOnly(2021, 9, 28))
+            };
+            await base.OnInitializedAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _module = await JS.InvokeAsync<IJSObjectReference>("import",
+                    "./js/dialogs.js");
+            }
+        }
+
+        async Task SaveFilm()
+        {
+            await _module.InvokeAsync<string>("hideModalWithId", "saveFilmModal");
+        }
+
+        async Task EditClicked(FilmDto film)
+        {
+            _filmToSave = film;
+            await _module.InvokeAsync<string>("showModalWithId", "saveFilmModal");
+        }
+
+        async Task AddClicked()
+        {
+            _filmToSave = new FilmDto
+            {
+                ReleaseDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+            await _module.InvokeAsync<string>("showModalWithId", "saveFilmModal");
+        }
+    }
+}
